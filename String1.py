5b99e02bc249afbbc395ea6d67fbc2438408a695
mikaizbaki <mikaizbaki@gmail.com>
2025-11-19 18:46:48 -0500

    String1.py

---
+++ String1.py
@@ -0,0 +1,108 @@
+# A. donuts
+# Given an int count of a number of donuts, return a string
+# of the form 'Number of donuts: <count>', where <count> is the number
+# passed in. However, if the count is 10 or more, then use the word 'many'
+# instead of the actual count.
+# So donuts(5) returns 'Number of donuts: 5'
+# and donuts(23) returns 'Number of donuts: many'
+def donuts(count):
+  def donuts(count):
+    if count >= 10:
+        return 'Number of donuts: many'
+    else:
+        return 'Number of donuts: ' + str(count)
+    
+     # B. both_ends
+# Given a string s, return a string made of the first 2
+# and the last 2 chars of the original string,
+# so 'spring' yields 'spng'. However, if the string length
+# is less than 2, return instead the empty string.
+def both_ends(s):
+    if len(s) < 2:
+        return ''
+    return s[:2] + s[-2:]
+
+# C. fix_start
+# Given a string s, return a string
+# where all occurences of its first char have
+# been changed to '*', except do not change
+# the first char itself.
+# e.g. 'babble' yields 'ba**le'
+# Assume that the string is length 1 or more.
+# Hint: s.replace(stra, strb) returns a version of string s
+# where all instances of stra have been replaced by strb.
+def fix_start(s):
+    first = s[0]              
+    rest = s[1:]              
+    fixed_rest = rest.replace(first, '*')  
+    return first + fixed_rest              
+  
+  # D. MixUp
+# Given strings a and b, return a single string with a and b separated
+# by a space '<a> <b>', except swap the first 2 chars of each string.
+# e.g.
+#   'mix', pod' -> 'pox mid'
+#   'dog', 'dinner' -> 'dig donner'
+# Assume a and b are length 2 or more.
+def mix_up(a, b):
+    a_new = b[:2] + a[2:]   
+    b_new = a[:2] + b[2:]
+    return a_new + " " + b_new
+
+# Provided simple test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+
+
+# Provided main() calls the above functions with interesting inputs,
+# using test() to check if each result is correct or not.
+def main():
+  print('donuts')
+  # Each line calls donuts, compares its result to the expected for that call.
+  test(donuts(4), 'Number of donuts: 4')
+  test(donuts(9), 'Number of donuts: 9')
+  test(donuts(10), 'Number of donuts: many')
+  test(donuts(99), 'Number of donuts: many')
+
+  print()
+  print('both_ends')
+  test(both_ends('spring'), 'spng')
+  test(both_ends('Hello'), 'Helo')
+  test(both_ends('a'), '')
+  test(both_ends('xyz'), 'xyyz')
+
+  # Provided simple test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+
+
+# Provided main() calls the above functions with interesting inputs,
+# using test() to check if each result is correct or not.
+def main():
+  print('donuts')
+  # Each line calls donuts, compares its result to the expected for that call.
+  test(donuts(4), 'Number of donuts: 4')
+  test(donuts(9), 'Number of donuts: 9')
+  test(donuts(10), 'Number of donuts: many')
+  test(donuts(99), 'Number of donuts: many')
+
+  print()
+  print('both_ends')
+  test(both_ends('spring'), 'spng')
+  test(both_ends('Hello'), 'Helo')
+  test(both_ends('a'), '')
+  test(both_ends('xyz'), 'xyyz')
+
+
+
+

39242a459818dce78a80ddc3bf3877f66d9a6b7f
mikaizbaki <mikaizbaki@gmail.com>
2025-11-27 12:43:27 -0500

    Created temperature.py

---
+++ temperature.py
@@ -0,0 +1,24 @@
+# program that converts temperature from celsius to fahrenheit
+
+# step 1. ask for user input for the temperature in celsius
+temperature = int(input("What is the temperature in Celsius? "))
+
+# step 2. convert the temperature from celsius to fahrenheit and assign it to a variable "new_temp"
+new_temp = (temperature*1.8)+32
+
+# step 3. print out "The temperature in Fahrenheit is: ..."
+print("The temperature in Farenheit is:")
+print(new_temp)
+
+# data types
+# integer = 123, 123, 213, 4
+# string = "helo", "123", "';l';l;k"
+# boolean = True, False
+# float = 123.123123, 1.34134, 0.45234
+
+# variable = where data is STORED
+
+# name = input("What's your name? ")
+# print(name)
+
+# typecasting

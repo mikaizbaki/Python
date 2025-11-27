98b90b6a4e05b928168d282866b22ecb429cad3e
mikaizbaki <mikaizbaki@gmail.com>
2025-11-27 13:06:37 -0500

    modified text in todo_list.py

---
+++ personal_projects/todo_list.py
@@ -0,0 +1,9 @@
+# step 1. create a list
+
+# step 2. ask how many things you need in your list
+
+# step 3. make a loop (depending on how many things you need)
+    # inside of the loop, ask each time for what to add to your todo list
+    # add the thing to your todo list
+
+# step 4. print out your todo list

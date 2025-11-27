ac8c9fff2c2706f562fbc73193d95f3c09aa8370
mikaizbaki <mikaizbaki@gmail.com>
2025-11-27 13:06:15 -0500

    added new project

---
+++ personal_projects/temperature_f_to_c.py
@@ -0,0 +1,8 @@
+# step 1. ask for user input for temperature in fahrenheit
+
+# step 2. convert the temperature from fahrenheit to celsius
+
+# step 3. print out the new temperature in celsius
+
+
+# C = (F - 32) * (5/9)

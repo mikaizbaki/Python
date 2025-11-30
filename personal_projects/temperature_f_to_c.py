ea21ae20256662db1674a21d9b09beabbaa1aac4
mikaizbaki <mikaizbaki@gmail.com>
2025-11-30 16:01:07 -0500

    Temperature_f_to_c created

--- personal_projects/temperature_f_to_c.py
+++ personal_projects/temperature_f_to_c.py
@@ -1,8 +1,13 @@
 # step 1. ask for user input for temperature in fahrenheit
+temperature = int(input("What is the temperature in Farenheit? "))
 
 # step 2. convert the temperature from fahrenheit to celsius
 
+new_temp = (temperature-32)*(5/9)
+
 # step 3. print out the new temperature in celsius
 
+print("The temperature in Celsius is:")
+print(new_temp)
 
 # C = (F - 32) * (5/9)
